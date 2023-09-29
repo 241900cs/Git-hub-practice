5e2b020c7f108753dbaf62af18655085a2b5ed93
241900cs <143010049+241900cs@users.noreply.github.com>
2023-09-29 14:01:13 -0400

    turtles in class

---
+++ git prac/MyClass.java
@@ -0,0 +1,32 @@
+public class MyClass{
+    public static void main (String[] args){
+        //String name = "christian";
+        //int size2 = 10;
+        //System.out.print(size2);
+       // boolean testBools = true;
+        /*if (testBools) = true;{
+            System.out.print("hello");
+        }*/
+        int r=7;
+        double circleArea;
+        circleArea = Math.PI*Math.pow(r, 2);
+        circleArea = 200;
+        System.out.println(circleArea+1);
+        circleArea += 100; //shortcut to add. removes repeated v
+
+
+
+        //How does division word??
+       double x = 4;
+        int y = 3;
+        int z = 2;
+        System.out.println(x/y);
+
+
+
+        //How does the moodulo (%) work??
+        System.out.println(y%z);
+
+    
+    }
+}
